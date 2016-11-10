ac997a0756918df4f86eade02e0f7b776f4dc066
mavyrick <jrsorokin@gmail.com>
2016-11-10 15:20:31 -0800

    Fix JQuery

--- app/assets/javascripts/application_questions.js
+++ app/assets/javascripts/application_questions.js
@@ -1,8 +1,8 @@
-$(document).ready(function() {
-  $('#application_questions').sortable({
-    update: function() {
-      var ids = $(this).sortable('serialize');
-      $.post('/companies/'+companyId+'/application_questions/sort', ids);
-    }
-  });
-});
+// $(document).ready(function() {
+//   $('#application_questions').sortable({
+//     update: function() {
+//       var ids = $(this).sortable('serialize');
+//       $.post('/companies/'+companyId+'/application_questions/sort', ids);
+//     }
+//   });
+// });
